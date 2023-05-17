34176917ad9421326f7bb4461f3f241c6f19f500
Robson Herminio <robsonhb@uninove.br>
2023-05-17 19:09:35 -0300

    Finalizando a criação das Migrations

---
+++ database/migrations/2023_05_17_191808_create_produto_detalhes_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProdutoDetalhesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produto_detalhes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('produto_id'); //produto_id => utilizamos o singular da tabela que será referencia para a chave estrangeira
+            $table->float('comprimento', 8, 2);
+            $table->float('largura', 8, 2);
+            $table->float('altura', 8, 2);
+            $table->timestamps();
+
+            //constraint (Indica que o campo produto_id referencia o campo id da tabela produtos)
+            $table->foreign('produto_id')->references('id')->on('produtos');
+            //Indica que o campo abaixo é único
+            $table->unique('produto_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produto_detalhes');
+    }
+}
